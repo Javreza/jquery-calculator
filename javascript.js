c2e10260f83ee9681506bd47668679b2c3caf808
jvrza <jvrza20@gmail.com>
2023-11-29 12:38:41 -0600

    Added a whole lot... lol

---
+++ javascript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var result = 0;
+    var prevEntry = 0;
+    var operation = null;
+    var currentEntry ='0';
+    updateScreen(result);
+
+    $('.button').on('click')
+})
